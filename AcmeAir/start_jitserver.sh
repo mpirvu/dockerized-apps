2409ae0d8f02e8f73e3e86067884a247e379f656
Marius Pirvu <mpirvu@ca.ibm.com>
2022-03-19 03:15:32 +0000

    Fixed a typo in the README file
    Also added more explanations in the README
    
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- AcmeAir/start_jitserver.sh
+++ AcmeAir/start_jitserver.sh
@@ -1,2 +1,2 @@
-docker run -d --rm --network=mynet -m=2G --cpus=4  -p 38400:38400 -e _JAVA_OPTIONS="" --name jitserver jitserver:11
+docker run -d --rm --network=mynet -m=2G --cpus=4  -p 38400:38400 -e _JAVA_OPTIONS="-XX:+JITServerLogConnections" --name jitserver jitserver:11
 
