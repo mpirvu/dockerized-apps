efd139a88c3a58d2e668cb9c8b861375f00ff824
root <root@JITServerRHEL1.fyre.ibm.com>
2023-03-09 08:31:21 -0800

    Dockerized version of AcmeAir EE8
    Signed-off-by: root <root@JITServerRHEL1.fyre.ibm.com>

---
+++ AcmeAirEE8/loadDatabase.sh
@@ -0,0 +1,2 @@
+curl --ipv4 -v http://localhost:9080/rest/info/loader/load?numCustomers=10000
+
---
+++ AcmeAirEE8/startAcmeAir.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Note: a mongo-acmeair image must already exist on the system
+ACMEAIR_IMAGE_NAME="liberty-acmeair-ee8:J17"
+NET_NAME="my-net"
+
+function startMongo()
+{
+docker run --rm -d --name mongodb --network=$NET_NAME mongo:5.0.15 --nojournal && sleep 1
+}
+function stopMongo()
+{
+  echo "Stopping mongodb container ..."
+  docker stop mongodb
+}
+
+docker network create $NET_NAME
+networkCreated=$?
+stopMongo
+sleep 1
+startMongo
+if [[ $? -eq 0 ]]; then
+  # -e MONGO_MANUAL="true"
+docker run --rm -it -m=1G --cpus=4.0 --network=$NET_NAME --name acmeair -p 9080:9080 -e MONGO_HOST="mongodb" -e MONGO_PORT="27017" -e MONGO_DBNAME="acmeair" $ACMEAIR_IMAGE_NAME
+  stopMongo
+else
+  echo "liberty-acmeair container not created"
+fi
+# If a new network was created, delete it now
+if [[ $networkCreated -eq 0 ]]; then
+  echo "Deleting docker network $NET_NAME"
+docker network rm $NET_NAME
+fi
+
